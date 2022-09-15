bb817cacc202c1697a156fafca84cfa4eacdb0c5
641i130 <galileoray@gmail.com>
2022-09-15 18:37:34 -0500

    Download with progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,53 @@
-use reqwest::header;
-use reqwest::blocking::Client;
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut headers = header::HeaderMap::new();
-    //headers.insert("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0".parse().unwrap());
-    //headers.insert("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8".parse().unwrap());
-    //headers.insert("Accept-Language", "en-US,en;q=0.5".parse().unwrap());
-    //headers.insert("Accept-Encoding", "gzip, deflate, br".parse().unwrap());
-    //headers.insert("Connection", "keep-alive".parse().unwrap());
+use std::cmp::min;
+use std::fs::File;
+use std::io::Write;
 
+use reqwest::Client;
+use indicatif::{ProgressBar, ProgressStyle};
+use futures_util::StreamExt;
+
+pub async fn download_file(client: &Client, url: &str, path: &str) -> Result<(), String> {
+    // Reqwest setup
+    let res = client
+        .get(url)
+        .send()
+        .await
+        .or(Err(format!("Failed to GET from '{}'", &url)))?;
+    let total_size = res
+        .content_length()
+        .ok_or(format!("Failed to get content length from '{}'", &url))?;
+    
+    // Indicatif setup
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("#>-"));
+    pb.set_message(&format!("Downloading {}", url));
+
+    // download chunks
+    let mut file = File::create(path).or(Err(format!("Failed to create file '{}'", path)))?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err(format!("Error while downloading file")))?;
+        file.write_all(&chunk)
+            .or(Err(format!("Error while writing to file")))?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
+    }
+
+    pb.finish_with_message(&format!("Downloaded {} to {}", url, path));
+    return Ok(());
+}
+
+#[tokio::main]
+async fn main() {
+    let url = "https://discord.com/api/download?platform=linux&format=tar.gz";
     let client = Client::new();
-    let res = client.get("https://discord.com/api/download?platform=linux&format=tar.gz")
-        .headers(headers)
-        .send()?
-        .text()?;
-    //res
+    let path = "./discord.tar.gz";
+    // Download newest tar
+    download_file(&client,&url,&path).await; 
 
-    Ok(())
 }
